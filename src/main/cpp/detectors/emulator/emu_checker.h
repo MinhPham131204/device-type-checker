cf559e440959968332ef8bbbdd0cadd0509ec562
minh <pqminh131204@gmail.com>
2026-07-15 11:02:10 +0700

    feat(emu-check): implement main function of EmulatorChecker

--- src/main/cpp/detectors/emulator/emu_checker.h
+++ src/main/cpp/detectors/emulator/emu_checker.h
@@ -15,21 +15,21 @@ public:
     /**
      * main function to make a comprehensive analysis to detect emulator.
      */
-    void analyzeRiskLevel(std::vector<std::string>& evidences);
+    static void analyzeRiskLevel(std::vector<std::string>& evidences);
 
 private:
     /**
      * check System Properties (ro.kernel.qemu, ro.hardware, ro.build.flavor,...)
      */
-    bool checkSysProp(std::vector<std::string>& evidences);
+    static bool checkSysProp(std::vector<std::string>& evidences);
 
     /**
      * check files system (/dev/qemu_pipe, /dev/socket/genyd...)
      */
-    bool checkFileSystem(std::vector<std::string>& evidences);
+    static bool checkFileSystem(std::vector<std::string>& evidences);
 
     /**
      * check telephony (FEATURE_TELEPHONY)
      */
-    bool checkTelephony(std::vector<std::string>& evidences);
+    static bool checkTelephony(std::vector<std::string>& evidences);
 };
