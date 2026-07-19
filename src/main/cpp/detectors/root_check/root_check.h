09a03cdaa268837bf3fd25b2453b1fc5c07255b0
minh <pqminh131204@gmail.com>
2026-07-19 16:35:10 +0700

    feat(root-check): implement main function of RootChecker

--- src/main/cpp/detectors/root_check/root_check.h
+++ src/main/cpp/detectors/root_check/root_check.h
@@ -12,21 +12,21 @@ public:
     /**
      * main function to make a comprehensive analysis to detect emulator.
      */
-    void analyzeRiskLevel(std::vector<std::string>& evidences);
+    static void analyzeRiskLevel(std::vector<std::string>& evidences);
 
 private:
     /**
      * check files typically found on rooted devices
      */
-    void checkFileExist(std::vector<std::string>& evidences);
+    static void checkFileExist(std::vector<std::string>& evidences);
 
     /**
      * Probe for known root manager packages
      */
-    void checkInstallPkg(std::vector<std::string>& evidences);
+    static void checkInstallPkg(std::vector<std::string>& evidences);
 
     /**
      * Check signs of test builds and custom ROMs
      */
-    void checkCustomBuilds(std::vector<std::string>& evidences);
+    static void checkCustomBuilds(std::vector<std::string>& evidences);
 };
